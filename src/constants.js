e9d947b9047c38ee5898542f0817e38fe260abe9
Raul Cote <42467898+RaulCote@users.noreply.github.com>
2020-11-22 23:20:30 +0100

    Delete constants.js
    Better to keep it safe.

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-/* TODO: this key should be kept private, as it's intended that someone
-unknown could run this locally I will leave it open for a while */
-
-export const API_KEY = 'AIzaSyCo-tYlBZFB9dXRlYAOol00sbfizcA4bhY';
