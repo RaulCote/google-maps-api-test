70e359f12c2bb535dd6679387eedac0bac3e1525
Raul <raulcote@hotmail.com>
2020-11-22 13:22:37 +0100

    Map.css added and SearchMap.js for the search box.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import SearchMap from './features/map/SearchMap';
+import MapProvider from './features/map/Map';
+import { API_KEY } from './constants';
 
 function App() {
   return (
     <div className="App">
-      
+      <MapProvider apiKey={API_KEY} style={{ width: '100%', height: '100vh' }}>
+        <SearchMap />
+      </MapProvider>
     </div>
   );
 }
