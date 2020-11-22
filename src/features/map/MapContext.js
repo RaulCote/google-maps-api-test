510b73f7e9f13e618b8bda112ef7393728720778
Raul <raulcote@hotmail.com>
2020-11-22 17:04:45 +0100

    Refactored MapContext.js. Now it has a real state for the GoogleMaps objects (right now map itself + place) to allow to separate the concepts better.

--- src/features/map/MapContext.js
+++ src/features/map/MapContext.js
@@ -1,11 +1,79 @@
-import { useContext, createContext } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useReducer,
+} from 'react';
+
+/* This context and useMap API have been created
+  in order to have map and place objects in a global state for the maps, 
+  Redux complains with complex items like those but Context can work
+  perfectly. It lets me seperate the concepts better. As an example
+  thanks to it I can have Markers into a separate file and not together 
+  with Autocomplete search box */
 
 const MapContext = createContext(null);
 
-export function useMap() {
-  const map = useContext(MapContext);
+const MAP = 'MAP';
+const PLACE = 'PLACE';
 
-  return map;
+const mapContextReducer = (state, action) => {
+  switch (action.type) {
+    case MAP:
+      return {
+        ...state,
+        map: action.payload,
+      };
+    case PLACE:
+      return {
+        ...state,
+        place: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const MapProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(mapContextReducer, {
+    map: null,
+    place: null,
+  });
+
+  return (
+    <MapContext.Provider value={{ state, dispatch }}>
+      {children}
+    </MapContext.Provider>
+  );
+};
+
+export function useMap() {
+  const {
+    state: { map, place },
+    dispatch,
+  } = useContext(MapContext);
+
+  const loadMap = useCallback(
+    map => {
+      dispatch({
+        type: MAP,
+        payload: map,
+      });
+    },
+    [dispatch]
+  );
+
+  const updatePlace = useCallback(
+    place => {
+      dispatch({
+        type: PLACE,
+        payload: place,
+      });
+    },
+    [dispatch]
+  );
+
+  return { map, place, loadMap, updatePlace };
 }
 
-export default MapContext;
+export default MapProvider;
